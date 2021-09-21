af0c6ad393efff306bf06d02746323c090089cca
madhavs06 <smallsanju@gmail.com>
2021-09-21 06:53:50 -0700

    mlflow begins

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -74,8 +74,12 @@ def api_response(dict_request):
             response= predict(data)
             response= {"response": response}
             return response
-    except Exception as e:
+    except NotInRange as e:
         response= {"expected_range": get_schema(), "response": str(e)}
         return response
-
-    
+    except NotInCols as e:
+        response= {"expected_columns": get_schema().keys(), "response": str(e)}
+        return response
+    except Exception as e:
+        response = {"response": str(e) }
+        return response
